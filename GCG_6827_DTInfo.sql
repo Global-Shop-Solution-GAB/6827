d826f5ec36c1a776c7291f30f79b9c1809dc4770
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_DTInfo.sql

---
+++ GCG_6827_DTInfo.sql
@@ -0,0 +1,12 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_DTInfo" (
+ "Router" CHAR(20),
+ "PriceDesc" CHAR(10),
+ "Price" DOUBLE,
+ "MarkUp" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
