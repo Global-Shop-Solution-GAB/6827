8f432af86cb87a08d9601e605399ddd2483d3f44
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_DTLIST.sql

---
+++ GCG_6827_DTLIST.sql
@@ -0,0 +1,21 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_DTLIST" (
+ "Router" CHAR(20),
+ "Part" CHAR(20),
+ "Qty" DOUBLE,
+ "ListLbs" DOUBLE,
+ "List_Time_HR_One" DOUBLE,
+ "List_Time_HR_Lot" DOUBLE,
+ "ListCost" DOUBLE,
+ "ListPrice" DOUBLE,
+ "ListGroupNotes" LONGVARCHAR,
+ "ListLineNotes" LONGVARCHAR,
+ "CalcW" DOUBLE,
+ "CalcL" DOUBLE,
+ "LineID" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
