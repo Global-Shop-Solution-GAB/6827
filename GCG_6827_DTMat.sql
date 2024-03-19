7f8c67f73043a7d5643d94867c677b245054dbb0
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_DTMat.sql

---
+++ GCG_6827_DTMat.sql
@@ -0,0 +1,21 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_DTMat" (
+ "Router" CHAR(20),
+ "Width" DOUBLE,
+ "Length" DOUBLE,
+ "Weight" DOUBLE,
+ "Cost_Lbs" DOUBLE,
+ "Cost_PO" DOUBLE,
+ "Material_Type" CHAR(10),
+ "Shape" CHAR(1),
+ "Thickness" DOUBLE,
+ "UOM" CHAR(2),
+ "Material_Used" DOUBLE,
+ "Price_Lbs" DOUBLE,
+ "FaceArea" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
