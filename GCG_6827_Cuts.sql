f4bffce730e92ea10f9b923194d0b1c5e31fe5d5
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_Cuts.sql

---
+++ GCG_6827_Cuts.sql
@@ -0,0 +1,13 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_Cuts" (
+ "Thickness" CHAR(50),
+ "Inches" DOUBLE,
+ "CutType" CHAR(50),
+ "Material" CHAR(50),
+ "Mat_Value" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
