5f7eb37fe9af02cd928761095da8714ec22e9bf8
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_DTMethod.sql

---
+++ GCG_6827_DTMethod.sql
@@ -0,0 +1,21 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_DTMethod" (
+ "Router" CHAR(20),
+ "Method" CHAR(4),
+ "Method_Name" CHAR(30),
+ "Setup" DOUBLE,
+ "Minimum" DOUBLE,
+ "Cost_WC" DOUBLE,
+ "Cost_One" DOUBLE,
+ "Cost_Lot" DOUBLE,
+ "Price_WC" DOUBLE,
+ "Price_One" DOUBLE,
+ "Price_Lot" DOUBLE,
+ "Time_HR_One" DOUBLE,
+ "Time_HR_Lot" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
