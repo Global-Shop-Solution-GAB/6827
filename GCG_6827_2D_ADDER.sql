397469223d9e51b7decdb620ce2aca3b212d0d77
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_2D_ADDER.sql

---
+++ GCG_6827_2D_ADDER.sql
@@ -0,0 +1,11 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_2D_ADDER" (
+ "Thickness" DOUBLE,
+ "Adder" DOUBLE,
+ "Correction" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
