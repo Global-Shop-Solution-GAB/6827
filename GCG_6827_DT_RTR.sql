70ea85421d6ae98fea8136daaa69e34c1075e7db
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_DT_RTR.sql

---
+++ GCG_6827_DT_RTR.sql
@@ -0,0 +1,23 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_DT_RTR" (
+ "Router" CHAR(20),
+ "Seq" CHAR(6),
+ "Part" CHAR(20),
+ "Description" CHAR(30),
+ "LMO" CHAR(1),
+ "Qty" DOUBLE,
+ "UOM" CHAR(2),
+ "Cost_UOM" DOUBLE,
+ "Cost_One" DOUBLE,
+ "Cost_Lot" DOUBLE,
+ "Price_UOM" DOUBLE,
+ "Price_One" DOUBLE,
+ "Price_Lot" DOUBLE,
+ "Note" LONGVARCHAR,
+ "Vendor" CHAR(6) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
