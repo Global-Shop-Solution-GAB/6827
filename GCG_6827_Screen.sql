e7d921acebee6e152234c11036b3338c65b15cb2
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_Screen.sql

---
+++ GCG_6827_Screen.sql
@@ -0,0 +1,30 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_Screen" (
+ "Q_O" CHAR(1),
+ "Part" CHAR(20),
+ "RawMat" CHAR(20),
+ "Description" CHAR(30),
+ "LineType" CHAR(6),
+ "Method" CHAR(10),
+ "Qty" DOUBLE,
+ "Width" DOUBLE,
+ "Length" DOUBLE,
+ "InchesOfCut" DOUBLE,
+ "PricingType" CHAR(10),
+ "InvUoM" CHAR(2),
+ "OverSized" CHAR(1),
+ "CutType" CHAR(10),
+ "TotalQty" DOUBLE,
+ "TotalCost" DOUBLE,
+ "MarkUp" CHAR(10),
+ "TotalPrice" DOUBLE,
+ "TotalGross" DOUBLE,
+ "Global_Total_Price" DOUBLE,
+ "Mat_Dim" CHAR(1),
+ "Cust" CHAR(6) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
