f7641e77b0a59f3fe16ceb5adf197ebe292b23dc
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_DTOutside.sql

---
+++ GCG_6827_DTOutside.sql
@@ -0,0 +1,19 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_DTOutside" (
+ "Router" CHAR(20),
+ "Method" CHAR(4),
+ "MethodDesc" CHAR(30),
+ "Method_Type" CHAR(20),
+ "Vendor" CHAR(6),
+ "VendorName" CHAR(30),
+ "TotalCost" DOUBLE,
+ "TotalPrice" DOUBLE,
+ "Cost" DOUBLE,
+ "Price" DOUBLE,
+ "MarkUp" DOUBLE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
