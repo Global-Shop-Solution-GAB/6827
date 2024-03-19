01c7c4581394571ad2aa8cec4767a6b71c37c665
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_METHOD.sql

---
+++ GCG_6827_METHOD.sql
@@ -0,0 +1,17 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_METHOD" (
+ "Method" CHAR(20),
+ "Method_Name" CHAR(30),
+ "Method_Type" CHAR(20),
+ "Dept" CHAR(20),
+ "Setup" DOUBLE,
+ "Minimum" DOUBLE,
+ "RuntimeMultiplier" DOUBLE,
+ "D_One" BIT NOT NULL,
+ "D_Two" BIT NOT NULL );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
