821665758f3ba8835413a62f2b21ae51f7d52776
Service Web <sw@gssmail.com>
2024-03-19 13:58:10 -0500

    Commit changes in C:\Websites\Git\emarlow\6827\GCG_6827_2D_PERCENT.sql

---
+++ GCG_6827_2D_PERCENT.sql
@@ -0,0 +1,13 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Jan 25, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6827_2D_PERCENT" (
+ "Width" DOUBLE,
+ "Length" DOUBLE,
+ "Area" DOUBLE,
+ "Percentage" DOUBLE,
+ "PL" CHAR(2) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
